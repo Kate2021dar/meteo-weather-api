4756598002eaf814cb817da6b38bfa2d5f397eda
Kate2021dar <smile555kd@gmail.com>
2022-08-14 21:52:25 +0300

    Add icon, celsius-fahrenheit

--- src/index.js
+++ src/index.js
@@ -54,6 +54,9 @@ function showTemperature(response) {
   let cityDescription = document.querySelector("#current-description");
   let cityHumidity = document.querySelector("#humidity");
   let cityWind = document.querySelector("#wind");
+  let cityIcon = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
 
   cityTemperature.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
@@ -62,7 +65,41 @@ function showTemperature(response) {
   cityDescription.innerHTML = response.data.weather[0].description;
   cityHumidity.innerHTML = response.data.main.humidity;
   cityWind.innerHTML = response.data.wind.speed;
+  cityIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  cityIcon.setAttribute("alt", response.data.weather[0].description);
 }
+
+function calculateFahrenheitTemperature(event) {
+  event.preventDefault();
+
+  let cityTemperature = document.querySelector("#id-temperature");
+  //remove and add the active class
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  cityTemperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function calculateCelsiusTemperature(event) {
+  event.preventDefault();
+  //remove and add the active class
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let cityTemperature = document.querySelector("#id-temperature");
+  cityTemperature.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#id-fahrenheit");
+fahrenheitLink.addEventListener("click", calculateFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#id-celsius");
+celsiusLink.addEventListener("click", calculateCelsiusTemperature);
 
 let apiKey = "41c8677f21e466c9b152647e17c8d1ac";
 let city = "Kharkiv";
